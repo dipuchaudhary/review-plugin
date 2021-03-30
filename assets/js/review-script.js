1eac62abb4afdc5b2d9b08881f45d5ebceed50d6
dipuchaudhary <dipuchaudhary8@gmail.com>
2021-03-30 18:11:40 +0545

    required fields appearing on all fields even just one field is empty fixed

--- assets/js/review-script.js
+++ assets/js/review-script.js
@@ -9,16 +9,49 @@ jQuery(document).ready( function($) {
         var pass = $("#password").val();
         var review = $("#review-desc").val();
         var rating = $("#rating").val();
-        
-        if( '' === fname || '' === lname || '' === email || '' === pass || '' === review || '' === rating ) {
+        $("#fname").closest(".form-group").find("#errfname").hide();
+        $("#lname").closest(".form-group").find("#errlname").hide();
+        $("#ename").closest(".form-group").find("#erremail").hide();
+        $("#password").closest(".form-group").find("#errpass").hide();
+        $("#review-desc").closest(".form-group").find("#errreview").hide();
+        $("#rating").closest(".form-group").find("#errrating").hide();
 
-            $("#errfname").text( translated_object.errfname );
-            $("#errlname").text( translated_object.errlname );
-            $("#erremail").text( translated_object.erremail );
-            $("#errpass").text( translated_object.errpass );
-            $("#errreview").text( translated_object.errreview );
-            $("#errrating").text( translated_object.errrating );
-        }
+        if( fname.length == 0 ) {
+            $("#fname").closest(".form-group").find("#errfname").addClass("active").html( translated_object.errfname );
+            $("#fname").closest(".form-group").find("#errfname").show();
+        } else {  
+            $("#fname").closest(".form-group").find("#errfname").hide();
+        } 
+        if( lname.length == 0 ) {
+            $("#lname").closest(".form-group").find("#errlname").addClass("active").html( translated_object.errlname );
+            $("#lname").closest(".form-group").find("#errlname").show();
+        } else { 
+            $("#lname").closest(".form-group").find("#errlname").hide();
+        } 
+        if( email.length == 0 ) {
+            $("#ename").closest(".form-group").find("#erremail").addClass("active").html( translated_object.email );
+            $("#ename").closest(".form-group").find("#erremail").show();
+        } else {  
+            $("#ename").closest(".form-group").find("#erremail").hide();
+        } 
+        if( pass.length == 0 ) {
+            $("#password").closest(".form-group").find("#errpass").addClass("active").html( translated_object.errpass );
+            $("#password").closest(".form-group").find("#errpass").show();
+        } else { 
+            $("#password").closest(".form-group").find("#errpass").hide();
+        } 
+        if( review.length == 0 ) {
+            $("#review-desc").closest(".form-group").find("#errreview").addClass("active").html( translated_object.errreview );
+            $("#review-desc").closest(".form-group").find("#errreview").show();
+        } else {  
+            $("#review-desc").closest(".form-group").find("#errreview").hide();
+        } 
+        if( rating.length == 0 ) {
+            $("#rating").closest(".form-group").find("#errrating").addClass("active").html( translated_object.errrating );
+            $("#rating").closest(".form-group").find("#errrating").show();
+        } else {  
+            $("#rating").closest(".form-group").find("#errrating").hide();
+        } 
             $.ajax({
                 url: myAjax.ajaxurl,
                 type: "POST",
